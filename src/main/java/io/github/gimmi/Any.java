450c085e7ff702384d397131d0fb5a7de33e1209
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-11-22 10:25:51 +0100

    Empty collections are equivalent to nulls

--- src/main/java/io/github/gimmi/Any.java
+++ src/main/java/io/github/gimmi/Any.java
@@ -1,8 +1,6 @@
 package io.github.gimmi;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.apache.commons.lang3.StringUtils.stripToEmpty;
-import static org.apache.commons.lang3.StringUtils.stripToNull;
+import org.apache.commons.lang3.BooleanUtils;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,115 +8,136 @@ import java.util.Collections;
 import java.util.TreeMap;
 import java.util.function.Consumer;
 
-import org.apache.commons.lang3.BooleanUtils;
+import static org.apache.commons.lang3.StringUtils.stripToNull;
 
 public class Any {
-	public static Any NULL = new Any(null, null, null);
-	
-	public static Any scalar(BigDecimal value) {
-		return new Any(value == null ? null : value.toPlainString(), null, null);
-	}
-	
-	public static Any scalar(Boolean value) {
-		return new Any(value == null ? null : value.toString(), null, null);
-	}
-	
-	public static Any scalar(String value) {
-		return new Any(value, null, null);
-	}
-	
-	public static Any map(Consumer<AnyMapBuilder> builder) {
-		AnyMapBuilder b = new AnyMapBuilder();
-		builder.accept(b);
-		return b.build();
-	}
+    public static Any NULL = new Any(null, null, null);
+    private final String scalar;
+    private final TreeMap<String, Any> map;
+    private final ArrayList<Any> list;
 
-	public static Any list(Consumer<AnyListBuilder> builder) {
-		AnyListBuilder b = new AnyListBuilder();
-		builder.accept(b);
-		return b.build();
-	}
-	
-	private final String scalar;
-	private final TreeMap<String, Any> map;
-	private final ArrayList<Any> list;
-	
-	protected Any(String scalar, TreeMap<String, Any> map, ArrayList<Any> list) {
-		this.scalar = scalar;
-		this.list = list;
-		this.map = map;
-	}
-	
-	public int size() {
-		if (map != null) {
-			return map.size();
-		} else if (list != null) {
-			return list.size();
-		}
-		return 0;
-	}
-	
-	public Any get(String key) {
-		key = stripToNull(key);
-		if (map != null && key != null) {
-			Any val = map.get(key);
-			if (val != null) {
-				return val;
-			}
-		}
-		return Any.NULL;
-	}
-	
-	public Any get(Integer index) {
-		if (list != null && index != null && index >= 0 && index < list.size()) {
-			return list.get(index);
-		}
-		return Any.NULL;
-	}
-	
-	@Override
-	public String toString() {
-		if (map != null) {
-			return "";
-		} else if (list != null) {
-			return "";
-		}
-		return stripToEmpty(scalar);
-	}
-	
-	public BigDecimal toBigDecimal() {
-		if (map != null) {
-			return BigDecimal.ZERO;
-		} else if (list != null) {
-			return BigDecimal.ZERO;
-		} else if (isBlank(scalar)) {
-			return BigDecimal.ZERO;
-		}
-		return new BigDecimal(scalar);
-	}
-	
-	public boolean toBoolean() {
-		if (map != null) {
-			return false;
-		} else if (list != null) {
-			return false;
-		}
-		return BooleanUtils.toBoolean(stripToEmpty(scalar));
-	}
-	
-	public Iterable<String> keys() {
-		if (map != null) {
-			return  map.keySet();
-		}
-		return Collections.emptySet();
-	}
+    protected Any(String scalar, TreeMap<String, Any> map, ArrayList<Any> list) {
+        this.scalar = scalar;
+        this.list = list;
+        this.map = map;
+    }
 
-	public Iterable<Any> values() {
-		if (map != null) {
-			return  map.values();
-		} else if (list != null) {
-			return list;
-		}
-		return Collections.emptyList();
-	}
+    public static Any scalar(BigDecimal value) {
+        if (value == null) {
+            return Any.NULL;
+        }
+        return new Any(value.toPlainString(), null, null);
+    }
+
+    public static Any scalar(Boolean value) {
+        if (value == null) {
+            return Any.NULL;
+        }
+        return new Any(value.toString(), null, null);
+    }
+
+    public static Any scalar(String value) {
+        value = stripToNull(value);
+        if (value == null) {
+            return Any.NULL;
+        }
+        return new Any(value, null, null);
+    }
+
+    public static Any map(Consumer<AnyMapBuilder> builder) {
+        AnyMapBuilder b = new AnyMapBuilder();
+        builder.accept(b);
+        return b.build();
+    }
+
+    public static Any list(Consumer<AnyListBuilder> builder) {
+        AnyListBuilder b = new AnyListBuilder();
+        builder.accept(b);
+        return b.build();
+    }
+
+    public int size() {
+        if (map != null) {
+            return map.size();
+        } else if (list != null) {
+            return list.size();
+        } else if (scalar != null) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public Any get(String key) {
+        key = stripToNull(key);
+        if (key == null) {
+            return Any.NULL;
+        } else if (map != null) {
+            Any val = map.get(key);
+            if (val != null) {
+                return val;
+            }
+        } else if (list != null) {
+            Integer index;
+            try {
+                index = Integer.parseInt(key);
+            } catch (NumberFormatException e) {
+                index = null;
+            }
+            return get(index);
+        }
+        return Any.NULL;
+    }
+
+    public Any get(Integer index) {
+        if (index == null) {
+            return Any.NULL;
+        } else if (list != null) {
+            if (index >= 0 && index < list.size()) {
+                return list.get(index);
+            }
+        } else if (map != null) {
+            return get(index.toString());
+        }
+        return Any.NULL;
+    }
+
+    @Override
+    public String toString() {
+        return getScalar("");
+    }
+
+    public BigDecimal toBigDecimal() {
+        return new BigDecimal(getScalar("0"));
+    }
+
+    public boolean toBoolean() {
+        return BooleanUtils.toBoolean(getScalar("false"));
+    }
+
+    public Iterable<String> keys() {
+        if (map != null) {
+            return map.keySet();
+        }
+        return Collections.emptySet();
+    }
+
+    public Iterable<Any> values() {
+        if (map != null) {
+            return map.values();
+        } else if (list != null) {
+            return list;
+        }
+        return Collections.emptyList();
+    }
+
+    private String getScalar(String def) {
+        if (map != null && map.size() > 0) {
+            return map.firstEntry().getValue().scalar;
+        } else if (list != null && list.size() > 0) {
+            return list.get(0).scalar;
+        } else if (scalar != null) {
+            return scalar;
+        }
+        return def;
+    }
 }
--- src/test/java/any/AnyTest.java
+++ src/test/java/any/AnyTest.java
@@ -1,118 +1,140 @@
 package any;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.StrictAssertions.assertThat;
+import io.github.gimmi.Any;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 
-import org.junit.Test;
-
-import io.github.gimmi.Any;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnyTest {
-	@Test
-	public void should_handle_string_scalar() {
-		assertThat(Any.scalar("hello world").toString()).isEqualTo("hello world");
-		assertThat(Any.scalar((String)null).toString()).isEqualTo("");
-		assertThat(Any.scalar("  ").toString()).isEqualTo("");
-		assertThat(Any.scalar(" x ").toString()).isEqualTo("x");
-		assertThat(Any.scalar("\n\trow1\n\trow2\n").toString()).isEqualTo("row1\n\trow2");
-	}
-	
-	@Test
-	public void should_handle_number_scalar() {
-		assertThat(Any.scalar(BigDecimal.valueOf(0)).toBigDecimal()).isEqualTo(BigDecimal.valueOf(0));
-		assertThat(Any.map(x -> x.put("key", Any.scalar("val"))).toBigDecimal()).isEqualTo(BigDecimal.ZERO);
-		assertThat(Any.list(x -> x.put(Any.scalar("3.14"))).toBigDecimal()).isEqualTo(BigDecimal.ZERO);
-		assertThat(Any.scalar(BigDecimal.valueOf(314, 2)).toBigDecimal()).isEqualTo(BigDecimal.valueOf(314, 2));
-		assertThat(Any.scalar((BigDecimal)null).toBigDecimal()).isEqualTo(BigDecimal.ZERO);
-		assertThat(Any.scalar("   ").toBigDecimal()).isEqualTo(BigDecimal.ZERO);
-	}
-	
-	@Test
-	public void should_handle_boolean_scalar() {
-		assertThat(Any.scalar(Boolean.valueOf(true)).toBoolean()).isTrue();
-		assertThat(Any.scalar(true).toBoolean()).isTrue();
-		assertThat(Any.scalar(Boolean.valueOf(false)).toBoolean()).isFalse();
-		assertThat(Any.scalar(false).toBoolean()).isFalse();
-		assertThat(Any.scalar((Boolean)null).toBoolean()).isFalse();
-		assertThat(Any.scalar("   ").toBoolean()).isFalse();
-		assertThat(Any.scalar(" true ").toBoolean()).isTrue();
-		assertThat(Any.scalar(" FALSE ").toBoolean()).isFalse();
-		assertThat(Any.scalar("").toBoolean()).isFalse();
-	}
-	
-	@Test
-	public void should_handle_empty_lists_like_null_objects() {
-		Any list = Any.list(l -> {});
-		Any nil = Any.NULL;
-		assertThat(list.size()).isEqualTo(nil.size());
-		assertThat(list.values()).containsExactlyElementsOf(nil.values());
-		assertThat(list.keys()).containsExactlyElementsOf(nil.keys());
-		assertThat(list.toString()).isEqualTo(nil.toString());
-		assertThat(list.toBoolean()).isEqualTo(nil.toBoolean());
-		assertThat(list.toBigDecimal()).isEqualTo(nil.toBigDecimal());
-		assertThat(list.get("x").toString()).isEqualTo(nil.get("x").toString());
-		assertThat(list.get((String)null).toString()).isEqualTo(nil.get((String)null).toString());
-		assertThat(list.get("  ").toString()).isEqualTo(nil.get("  ").toString());
-		assertThat(list.get(0).toString()).isEqualTo(nil.get(0).toString());
-}
-	
-	@Test
-	public void should_handle_lists_containing_values() {
-		Any list = Any.list(l -> {
-			l.put(Any.scalar("1"));
-			l.put(Any.scalar("2"));
-		});
-		
-		assertThat(list.size()).isEqualTo(2);
-		assertThat(list.values()).extracting(x -> x.toString()).containsExactly("1", "2");
-		assertThat(list.keys()).isEmpty();
-		assertThat(list.toString()).isEmpty();
-		assertThat(list.toBoolean()).isFalse();
-		assertThat(list.toBigDecimal()).isEqualByComparingTo(BigDecimal.ZERO);
-		assertThat(list.get(-1).toString()).isEmpty();
-		assertThat(list.get(0).toString()).isEqualTo("1");
-		assertThat(list.get(1).toString()).isEqualTo("2");
-		assertThat(list.get(2).toString()).isEmpty();
-		assertThat(list.get(100).toString()).isEmpty();
-		assertThat(list.get("x").toString()).isEmpty();
-	}
-	
-	@Test
-	public void should_handle_empty_maps_like_null_objects() {
-		Any map = Any.map(x -> {});
-		Any nil = Any.NULL;
-		assertThat(map.size()).isEqualTo(nil.size());
-		assertThat(map.values()).containsExactlyElementsOf(nil.values());
-		assertThat(map.keys()).containsExactlyElementsOf(nil.keys());
-		assertThat(map.toString()).isEqualTo(nil.toString());
-		assertThat(map.toBoolean()).isEqualTo(nil.toBoolean());
-		assertThat(map.toBigDecimal()).isEqualTo(nil.toBigDecimal());
-		assertThat(map.get("x").toString()).isEqualTo(nil.get("x").toString());
-		assertThat(map.get((String)null).toString()).isEqualTo(nil.get((String)null).toString());
-		assertThat(map.get("  ").toString()).isEqualTo(nil.get("  ").toString());
-		assertThat(map.get(0).toString()).isEqualTo(nil.get(0).toString());
-	}
-	
-	@Test
-	public void should_handle_map_containing_values() {
-		Any map = Any.map(x -> {
-			x.put("k1", Any.scalar("1"));
-			x.put("CAPITAL", Any.scalar("2"));
-			x.put(" k3 ", Any.scalar("3"));
-		});
-		assertThat(map.size()).isEqualTo(3);
-		assertThat(map.values()).extracting(x -> x.toString()).containsExactly("2", "1", "3");
-		assertThat(map.keys()).containsExactly("CAPITAL", "k1", "k3");
-		assertThat(map.toString()).isEmpty();
-		assertThat(map.toBoolean()).isFalse();
-		assertThat(map.toBigDecimal()).isEqualByComparingTo(BigDecimal.ZERO);
-		assertThat(map.get("x").toString()).isEmpty();
-		assertThat(map.get((String)null).toString()).isEmpty();
-		assertThat(map.get("  ").toString()).isEmpty();
-		assertThat(map.get(0).toString()).isEmpty();
-		assertThat(map.get("  k1  ").toString()).isEqualTo("1");
-		assertThat(map.get("  capital  ").toString()).isEqualTo("2");
-	}
+    @Test
+    public void should_handle_string_scalar() {
+        assertThat(Any.scalar("hello world").toString()).isEqualTo("hello world");
+        assertThat(Any.scalar((String) null).toString()).isEqualTo("");
+        assertThat(Any.scalar("  ").toString()).isEqualTo("");
+        assertThat(Any.scalar(" x ").toString()).isEqualTo("x");
+        assertThat(Any.scalar("\n\trow1\n\trow2\n").toString()).isEqualTo("row1\n\trow2");
+    }
+
+    @Test
+    public void should_handle_number_scalar() {
+        assertThat(Any.scalar(BigDecimal.valueOf(0)).toBigDecimal()).isEqualTo(BigDecimal.valueOf(0));
+        assertThat(Any.map(x -> x.put("key", Any.scalar("3.14"))).toBigDecimal()).isEqualTo(new BigDecimal("3.14"));
+        assertThat(Any.list(x -> x.put(Any.scalar("3.14"))).toBigDecimal()).isEqualTo(new BigDecimal("3.14"));
+        assertThat(Any.scalar(BigDecimal.valueOf(314, 2)).toBigDecimal()).isEqualTo(BigDecimal.valueOf(314, 2));
+        assertThat(Any.scalar((BigDecimal) null).toBigDecimal()).isEqualTo(BigDecimal.ZERO);
+        assertThat(Any.scalar("   ").toBigDecimal()).isEqualTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    public void should_handle_boolean_scalar() {
+        assertThat(Any.scalar(true).toBoolean()).isTrue();
+        assertThat(Any.scalar(false).toBoolean()).isFalse();
+        assertThat(Any.scalar((Boolean) null).toBoolean()).isFalse();
+        assertThat(Any.scalar("   ").toBoolean()).isFalse();
+        assertThat(Any.scalar(" true ").toBoolean()).isTrue();
+        assertThat(Any.scalar(" FALSE ").toBoolean()).isFalse();
+        assertThat(Any.scalar("").toBoolean()).isFalse();
+    }
+
+    @Test
+    public void should_handle_empty_lists_like_null_objects() {
+        Any list = Any.list(l -> {});
+        Any nil = Any.NULL;
+        assertThat(list.size()).isEqualTo(nil.size());
+        assertThat(list.values()).containsExactlyElementsOf(nil.values());
+        assertThat(list.keys()).containsExactlyElementsOf(nil.keys());
+        assertThat(list.toString()).isEqualTo(nil.toString());
+        assertThat(list.toBoolean()).isEqualTo(nil.toBoolean());
+        assertThat(list.toBigDecimal()).isEqualTo(nil.toBigDecimal());
+        assertThat(list.get("x").toString()).isEqualTo(nil.get("x").toString());
+        assertThat(list.get((String) null).toString()).isEqualTo(nil.get((String) null).toString());
+        assertThat(list.get("  ").toString()).isEqualTo(nil.get("  ").toString());
+        assertThat(list.get(0).toString()).isEqualTo(nil.get(0).toString());
+    }
+
+    @Test
+    public void should_handle_lists_containing_values() {
+        Any list = Any.list(l -> {
+            l.put(Any.scalar("1"));
+            l.put(Any.scalar("2"));
+        });
+
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.values()).extracting(Any::toString).containsExactly("1", "2");
+        assertThat(list.keys()).isEmpty();
+        assertThat(list.toString()).isEqualTo("1");
+        assertThat(list.toBoolean()).isFalse();
+        assertThat(list.toBigDecimal()).isEqualByComparingTo(new BigDecimal("1"));
+        assertThat(list.get(-1).toString()).isEmpty();
+        assertThat(list.get(0).toString()).isEqualTo("1");
+        assertThat(list.get(1).toString()).isEqualTo("2");
+        assertThat(list.get(2).toString()).isEmpty();
+        assertThat(list.get(100).toString()).isEmpty();
+        assertThat(list.get("x").toString()).isEmpty();
+    }
+
+    @Test
+    public void should_handle_empty_maps_like_null_objects() {
+        Any map = Any.map(x -> {});
+        Any nil = Any.NULL;
+        assertThat(map.size()).isEqualTo(nil.size());
+        assertThat(map.values()).containsExactlyElementsOf(nil.values());
+        assertThat(map.keys()).containsExactlyElementsOf(nil.keys());
+        assertThat(map.toString()).isEqualTo(nil.toString());
+        assertThat(map.toBoolean()).isEqualTo(nil.toBoolean());
+        assertThat(map.toBigDecimal()).isEqualTo(nil.toBigDecimal());
+        assertThat(map.get("x").toString()).isEqualTo(nil.get("x").toString());
+        assertThat(map.get((String) null).toString()).isEqualTo(nil.get((String) null).toString());
+        assertThat(map.get("  ").toString()).isEqualTo(nil.get("  ").toString());
+        assertThat(map.get(0).toString()).isEqualTo(nil.get(0).toString());
+    }
+
+    @Test
+    public void should_handle_map_containing_values() {
+        Any map = Any.map(x -> {
+            x.put("k1", Any.scalar("1"));
+            x.put("CAPITAL", Any.scalar("2"));
+            x.put(" k3 ", Any.scalar("3"));
+        });
+        assertThat(map.size()).isEqualTo(3);
+        assertThat(map.values()).extracting(Any::toString).containsExactly("2", "1", "3");
+        assertThat(map.keys()).containsExactly("CAPITAL", "k1", "k3");
+        assertThat(map.toString()).isEqualTo("2");
+        assertThat(map.toBoolean()).isFalse();
+        assertThat(map.toBigDecimal()).isEqualByComparingTo(new BigDecimal("2"));
+        assertThat(map.get("x").toString()).isEmpty();
+        assertThat(map.get((String) null).toString()).isEmpty();
+        assertThat(map.get("  ").toString()).isEmpty();
+        assertThat(map.get(0).toString()).isEmpty();
+        assertThat(map.get("  k1  ").toString()).isEqualTo("1");
+        assertThat(map.get("  capital  ").toString()).isEqualTo("2");
+    }
+
+    @Test
+    public void maps_should_act_as_lists() {
+        Any map = Any.map(x -> {
+            x.put("0", Any.scalar("zero"));
+            x.put("2", Any.scalar("two"));
+        });
+
+        assertThat(map.get(0).toString()).isEqualTo("zero");
+        assertThat(map.get(1).toString()).isEmpty();
+        assertThat(map.get(2).toString()).isEqualTo("two");
+        assertThat(map.get(3).toString()).isEmpty();
+    }
+
+    @Test
+    public void lists_should_get_by_string() {
+        Any map = Any.list(x -> {
+            x.put(Any.scalar("zero"));
+            x.put(Any.scalar("one"));
+        });
+
+        assertThat(map.get("0").toString()).isEqualTo("zero");
+        assertThat(map.get("+001").toString()).isEqualTo("one");
+        assertThat(map.get("2").toString()).isEmpty();
+        assertThat(map.get("not a number").toString()).isEmpty();
+    }
 }
