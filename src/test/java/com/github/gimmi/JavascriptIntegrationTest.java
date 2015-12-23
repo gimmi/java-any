ecc266eb077e891a49a076c418deb24bf7389c46
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2015-12-23 08:31:11 +0100

    Added integration test for Nashorn Javascript

---
+++ src/test/java/com/github/gimmi/JavascriptIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.github.gimmi;
+
+import org.junit.Test;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class JavascriptIntegrationTest {
+
+	@Test
+	public void should_work_from_js() throws ScriptException {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+		assertThat(engine.eval("com.github.gimmi.Any.of('str').val().orElse('');")).isEqualTo("str");
+	}
+}
